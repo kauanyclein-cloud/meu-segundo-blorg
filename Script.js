38f421fa167264cfc55da36c4612c4a68a7ce884
kauanyclein-cloud <kauany.clein@escola.pr.gov.br>
2026-08-12 09:26:51 -0300

    Add toggle feature for reaction button clicks
    Implement toggle functionality for reaction buttons.

--- Script.js
+++ Script.js
@@ -1,1 +1,21 @@
+// Seleciona todos os botões de reação da página
+const botoes = document.querySelectorAll(".btn-reacao");
 
+// Percorre cada botão individualmente
+botoes.forEach(function (botao) {
+  // Variável de controle individual de cada botão
+  let curtiu = false;
+
+  botao.addEventListener("click", function () {
+    let texto = botao.querySelector("span");
+
+    // Lógica para alternar entre curtir e descurtir (Toggle)
+    if (curtiu === false) {
+      texto.textContent++;
+      curtiu = true;
+    } else {
+      texto.textContent--;
+      curtiu = false;
+    }
+  });
+});
